05ce00f532712cab5595dfc9d7897fe2acd71d1a
CrisAlex <cads.14alex@outlook.com>
2022-10-02 11:28:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Pairs of integers (Sample input: 1,9,5,0,20,-4,12,16,7 12)");
+
+var result = Function.FindPairsOfIntegers(Console.ReadLine());
+foreach (var item in result) Console.WriteLine(item);
+
+Console.WriteLine("Press enter to exit!");
+Console.ReadLine();
+
+public static class Function
+{
+    public static IEnumerable<string> FindPairsOfIntegers(string input)
+    {
+        var line = input.Split(" ");
+        var list = new List<int>(line[0].Split(",").Select(x => int.Parse(x)));
+        var target = int.Parse(line[1]);
+
+        var map = new Dictionary<int, bool>(list.Select(x => new KeyValuePair<int, bool>(x, true)));
+        foreach (var value in list)
+        {
+            var targetValue = target - value;
+            if (map[value] && map.GetValueOrDefault(targetValue))
+            {
+                map[targetValue] = false;
+                yield return $"{value},{targetValue}";
+            }
+        }
+    }
+}
+
---
+++ Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace PairsOfIntegers
+{
+    public class Tests
+    {
+        [Test]
+        [TestCase("1,9,5,0,20,-4,12,16,7 12")]
+        [TestCase("-8,-2,4,7,3,11,17,6,-4,-5 9")]
+        [TestCase("-8,78,6,8,-79,1,0,31,7,-1 -1")]
+        [TestCase("4,0,-6,-14,12,-16,7,6,-2 17")]
+        public void Should_Pass_AllTestCases(string input)
+        {
+            //Arrange
+            var line = input.Split(" ");
+            var target = int.Parse(line[1]);
+            var list = new List<int>(line[0].Split(",").Select(x => int.Parse(x)));
+            var matches = new List<int>();
+            foreach (var value in list)
+            {
+                var targetValue = target - value;
+                if (!matches.Contains(value) && list.Contains(targetValue))
+                    matches.AddRange(new[] { value, targetValue });
+            }
+
+            //Act
+            var result = Function.FindPairsOfIntegers(input);
+
+            //Assert
+            var values = result.Any() ? string.Join(",", result)?.Split(",").Select(x => int.Parse(x)).ToList() : new List<int>();
+            foreach (var value in matches)
+            {
+                if (values.Contains(value)) values.Remove(value);
+            };
+            Assert.That(values.Count, Is.EqualTo(0));
+        }
+    }
+}
